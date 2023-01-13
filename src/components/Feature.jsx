ba86751b20a14c58d2266b80e99bdf192f9cce68
Zengoozz <classicalegyptian@gmail.com>
2023-01-13 21:49:52 +0200

    convert to dynamic styles component

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-export default function Feature({title, text, style, containerStyle}) {
+export default function Feature({title, text, paragraphStyle, containerStyle, titleStyle}) {
     return (
         <div className={`${containerStyle}`}>
             <div className='mb-[40px]'>
-                <div className='bar__gradient h-[3px] w-[40px] mb-[10px]'/>
-                <h1 className='text-white text-[25px] font-bold'>
+                <div className='bar__gradient h-[3px] w-[40px]'/>
+                <h1 className={titleStyle}>
                    {title}
                 </h1>
             </div>
-            <p className={`text-textCustom text-[18px] ${style}`}>
+            <p className={paragraphStyle}>
                 {text}
             </p>
         </div>
