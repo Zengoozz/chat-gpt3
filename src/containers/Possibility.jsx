70b0d1b1ad6312e6d10c8f999bfae9fa0da80787
Zengoozz <classicalegyptian@gmail.com>
2023-01-15 03:53:12 +0200

    possibility to mobile design

--- src/containers/Possibility.jsx
+++ src/containers/Possibility.jsx
@@ -3,19 +3,19 @@ import { possibility } from '../assets'
 
 export default function Possibility() {
     return (
-        <section id='possibility' className='p-[30px]'>
-            <div className='flex'>
-                <span className='w-1/2'>
+        <section id='possibility' className='ss:p-[30px] ss:mt-0 mt-[100px]'>
+            <div className='flex ss:flex-row flex-col-reverse'>
+                <span className='ss:w-1/2 ss:mt-0 mt-[20px]'>
                     <img src={possibility} alt="possibility VR" />
                 </span>
-                <div className='w-1/2 max-w-[470px] ml-[30px] mt-[230px]'>
-                    <h4 className='text-oneUseCustom mb-[20px]'>
+                <div className='ss:w-1/2 max-w-[470px] ss:ml-[30px] ss:mt-[230px]'>
+                    <h4 className='text-oneUseCustom ss:mb-[20px] mb-[10px]'>
                         Request Early Access to Get Started
                     </h4>
-                    <h1 className='gradient__text text-[35px] font-bold leading-[45px] mb-[30px]'>
+                    <h1 className='gradient__text text-[35px] font-bold leading-[45px] ss:mb-[30px] mb-[20px]'>
                         The possibilities are beyond your imagination
                     </h1>
-                    <p className='text-textCustom leading-[30px] mb-[30px]'>
+                    <p className='text-textCustom leading-[30px] ss:mb-[30px] mb-[20px]'>
                         Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.
                     </p>
                     <a  className='text-subTextCustom hover:underline'
