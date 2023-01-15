7801e9664f24514309e2f697add3151a97e8b2f6
Zengoozz <classicalegyptian@gmail.com>
2023-01-15 03:15:11 +0200

    navbar modify to mobile design

--- src/containers/Navbar.jsx
+++ src/containers/Navbar.jsx
@@ -37,18 +37,28 @@ export default function Navbar() {
                         Sign up
                     </button>
                 </div>
-                <div className='relative ss:hidden block'>
+                <div className='relative ss:hidden flex'>
+                    <div className='ss:hidden block mr-[20px]'>
+                        <button className='font-medium text-[15px]'>
+                            Sign in
+                        </button>
+                        <button className='ml-[20px] font-medium text-[15px] bg-buttonSignCustom 
+                            px-[10px] py-[5px] rounded-[5px]'>
+                            Sign up
+                        </button>
+                    </div>
                     <button onClick={() => setToggle(prevState => !prevState)}>
-                        <img src={toggle ? close : menu} alt="" />
+                        <img className='w-[30px] h-[30px] object-contain' 
+                            src={toggle ? close : menu} alt= {toggle ? "close" : "menu"} />
                     </button>
                     <div
-                        className={`card__gpt3 absolute right-[10px] top-[20px]
+                        className={`card__gpt3 absolute right-[10px] top-[40px] min-w-[150px] rounded-[10px] py-[10px] px-[20px]
                         ${toggle ? "flex flex-col items-center" : "hidden"}`}>
                         {
                             navLinks.map((link, index) => (
-                                <span className={`${index !== 0 ? "ml-[55px]" : "ml-0"}`}
+                                <span className={`${index !== 0 ? "mt-[15px]" : "mt-0"}`}
                                     key={link.id}>
-                                    <a className='font-medium text-[18px] hover:underline'
+                                    <a className='font-medium text-[18px]'
                                         href={`#${link.id}`}>
                                         {link.title}
                                     </a>
