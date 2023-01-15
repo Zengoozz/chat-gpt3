fc984030d90457380c9b7b9b1d03c5b29a624908
Zengoozz <classicalegyptian@gmail.com>
2023-01-15 03:30:56 +0200

    modify brands to mobile

--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -5,11 +5,11 @@ import { brandsLogos } from '../data/constants'
 
 export default function Brands() {
     return (
-        <section className='mt-[60px]'>
-            <div className='flex flex-1 justify-center'>
-                {brandsLogos.map((brand,index)=> (
-                    <span className={`${index !== 0 ? "ml-[50px]" : "ml-0"}`} 
-                    key={brand.id}>
+        <section className='ss:mt-[60px] mt-[30px]'>
+            <div className='flex flex-wrap ss:justify-center justify-around'>
+                {brandsLogos.map((brand, index) => (
+                    <span className={`${index !== 0 ? "ss:ml-[50px] ml-0" : "ml-0"} ss:mt-0 mt-[30px]`}
+                        key={brand.id}>
                         <img src={brand.img} alt={brand.id} />
                     </span>
                 ))}
