c5cd9ec37659db58bb1c4f98548fa64f0435c9f6
Zengoozz <classicalegyptian@gmail.com>
2023-01-15 04:06:00 +0200

    CTA to mobile design

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 export default function CTA() {
   return (
     <section id="CTA">
-      <div className='my-[150px]'>
-        <div className='flex items-center justify-between bar__gradient h-[100px] px-[50px] py-[80px] rounded-[10px]'>
+      <div className='ss:my-[150px] my-[50px] ss:text-left text-center'>
+        <div className='flex ss:flex-row flex-col flex-1 items-center ss:justify-between
+                  bar__gradient ss:p-[50px] p-[30px] rounded-[10px]'>
           <div>
             <p className='text-[12px] mb-[5px]'>
               Request Early Access to Get Started
@@ -13,7 +14,8 @@ export default function CTA() {
               Register today & start exploring the endless possiblities.
             </h2>
           </div>
-          <button className='bg-black text-white font-bold py-[15px] px-[40px] rounded-[40px] text-[18px]'>
+          <button className='bg-black text-white font-bold py-[15px] px-[40px] 
+                      rounded-[40px] text-[18px] ss:mt-0 mt-[10px]'>
             Get Started
           </button>
         </div>
