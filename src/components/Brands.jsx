19e611efea5604f1a946f0741523a61f0d00ece4
Zengoozz <classicalegyptian@gmail.com>
2023-01-17 10:47:39 +0200

    justifying brands mobile design position

--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -6,9 +6,17 @@ import { brandsLogos } from '../data/constants'
 export default function Brands() {
     return (
         <section className='ss:mt-[60px] mt-[30px]'>
-            <div className='flex flex-wrap ss:justify-center justify-around'>
+            <div className='ss:flex hidden flex-wrap justify-center'>
                 {brandsLogos.map((brand, index) => (
-                    <span className={`${index !== 0 ? "ss:ml-[50px] ml-0" : "ml-0"} ss:mt-0 mt-[30px]`}
+                    <span className={`${index !== 0 ? "ml-[50px]" : "ml-0"}`}
+                        key={brand.id}>
+                        <img src={brand.img} alt={brand.id} />
+                    </span>
+                ))}
+            </div>
+            <div className='ss:hidden flex flex-wrap justify-between'>
+                {brandsLogos.map((brand, index) => (
+                    <span className={`${index === 2 ? "w-full flex justify-center": ""} mt-[30px]`}
                         key={brand.id}>
                         <img src={brand.img} alt={brand.id} />
                     </span>
